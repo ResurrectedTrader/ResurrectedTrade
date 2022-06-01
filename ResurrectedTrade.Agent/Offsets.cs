359e204cc4eb7ac9b48125ee1d6766fec922b6cc
Resurrected Trader <admin@resurrected.trade>
2022-06-01 21:03:33 +0100

    Update offsets

--- ResurrectedTrade.Agent/Offsets.cs
+++ ResurrectedTrade.Agent/Offsets.cs
@@ -7,13 +7,13 @@ namespace ResurrectedTrade.Agent
     {
         public static readonly Offsets Instance = new Offsets();
 
-        public int SupportedVersion => 69640;
-        public Ptr UnitHashTable => 0x224B660;
-        public Ptr SessionData => 0x2438B08;
-        public Ptr Pets => 0x2266C58;
-        public Ptr UIState => 0x225B338;
-        public Ptr IsOnlineGame => 0x2107250;
-        public Ptr WidgetStates => 0x22837D8;
-        public Ptr CharFlags => 0x20FC8DB;
+        public int SupportedVersion => 69754;
+        public Ptr UnitHashTable => 0x226F6C0;
+        public Ptr SessionData => 0x245CB98;
+        public Ptr Pets => 0x228ACB8;
+        public Ptr UIState => 0x227F398;
+        public Ptr IsOnlineGame => 0x20EAED0;
+        public Ptr WidgetStates => 0x22A7838;
+        public Ptr CharFlags => 0x20E055B;
     }
 }
