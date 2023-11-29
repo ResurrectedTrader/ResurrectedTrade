3ccc3eea3af053fa52568682810d932f96722996
77920 <tyler@exclusiveprivatesale.com>
2023-11-29 16:03:44 -0500

    1.677312; Offsets //11.14.2023

--- ResurrectedTrade.Agent/Offsets.cs
+++ ResurrectedTrade.Agent/Offsets.cs
@@ -7,14 +7,14 @@ namespace ResurrectedTrade.Agent
     {
         public static readonly Offsets Instance = new Offsets();
 
-        public int SupportedVersion => 75728;
-        public Ptr UnitHashTable => 0x22a7000;
-        public Ptr SessionData => 0x2495bf8;
-        public Ptr Pets => 0x22c2618;
-        public Ptr InGame => 0x22B6CE8;
-        public Ptr IsOnlineGame => 0x2162250;
-        public Ptr WidgetStates => 0x22df250;
-        public Ptr CharFlags => 0x21578bb;
-        public Ptr LoadGameComplete => 0x2159E48;
+        public int SupportedVersion => 77312; //11.14.2023
+        public Ptr UnitHashTable => 0x22F20C0;
+        public Ptr SessionData => 0x24E0E78;
+        public Ptr Pets => 0x230D7D0;
+        public Ptr InGame => 0x2301DA0;
+        public Ptr IsOnlineGame => 0x21AD2D0;
+        public Ptr WidgetStates => 0x232A308;
+        public Ptr CharFlags => 0x21A293B;
+        public Ptr LoadGameComplete => 0x21A4EAC;
     }
 }
