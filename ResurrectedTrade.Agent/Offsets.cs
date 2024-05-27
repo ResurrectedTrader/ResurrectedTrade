f121ec02dd3fbe1c574f713e5a0c2db92ccca821
ResurrectedTrader <admin@resurrected.trade>
2024-05-27 14:11:13 +0100

    Update offsets for 80273

--- ResurrectedTrade.Agent/Offsets.cs
+++ ResurrectedTrade.Agent/Offsets.cs
@@ -7,14 +7,14 @@ namespace ResurrectedTrade.Agent
     {
         public static readonly Offsets Instance = new Offsets();
 
-        public int SupportedVersion => 77312; //11.14.2023
-        public Ptr UnitHashTable => 0x22F20C0;
-        public Ptr SessionData => 0x24E0E78;
-        public Ptr Pets => 0x230D7D0;
-        public Ptr InGame => 0x2301DA0;
-        public Ptr IsOnlineGame => 0x21AD2D0;
-        public Ptr WidgetStates => 0x232A308;
-        public Ptr CharFlags => 0x21A293B;
-        public Ptr LoadGameComplete => 0x21A4EAC;
+        public int SupportedVersion => 80273;
+        public Ptr UnitHashTable => 0x22DA110;
+        public Ptr SessionData => 0x24C8E60;
+        public Ptr Pets => 0x22F5728;
+        public Ptr InGame => 0x22E9DF8;
+        public Ptr IsOnlineGame => 0x2154F50;
+        public Ptr WidgetStates => 0x2312360;
+        public Ptr CharFlags => 0x214A5BB;
+        public Ptr LoadGameComplete => 0x214CB48;
     }
 }
